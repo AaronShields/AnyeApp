5ab3696d480aa6ea5656a88e4b02d1b20a363302
Aaron Shields <amshield@usc.edu>
2025-05-26 20:43:42 -0400

    form updates

--- client/src/userform.tsx
+++ client/src/userform.tsx
@@ -9,15 +9,17 @@ const UserForm: React.FC = () => {
     const [message, setMessage ] = useState(""); 
     const [phonenumber, setNumber] = useState(""); 
     const [feedback, setFeedback] = useState(""); 
-    const [category, setCategory] = useState("Love"); 
+    const [category, setCategory] = useState(""); 
+    const [carrier, setCarrier] = useState(""); 
     
     const handleSubmit = (e: React.FormEvent) => {
         e.preventDefault(); 
-        console.log("User Info:", name, message , phonenumber, category); 
+        console.log("User Info:", name, message , phonenumber, category, carrier); 
         setName(""); 
         setMessage(""); 
         setNumber(""); 
         setCategory(""); 
+        setCarrier(""); 
         setFeedback("Message Sent!"); 
     }; 
     return (
@@ -36,8 +38,18 @@ const UserForm: React.FC = () => {
             <label htmlFor="phonenumber">Phone Number:</label>
                 <input id="phoneNumber" type="text" value={phonenumber} onChange={(e) => setNumber(e.target.value)} />
         </div>
+
         <div className="form-row">
-            <label htmlFor="phonenumber">Category:</label>
+            <label htmlFor="carrier">Carrier:</label>
+            <select value={category} onChange={(e) => setCarrier(e.target.value)}>
+                <option value="Tmobile">Tmobile</option>
+                <option value="Sprint">Sprint</option>
+                <option value="Verizon">Verzion</option>
+            </select>
+        </div>
+    
+        <div className="form-row">
+            <label htmlFor="category">Category:</label>
             <select value={category} onChange={(e) => setCategory(e.target.value)}>
                 <option value="Love">Love</option>
                 <option value="Reminder">Reminder</option>
